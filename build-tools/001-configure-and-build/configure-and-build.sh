55b4ff4e8cdd679b007a288e802bff20457dec4b
Christian A. Jacobsen <christian@jenkins.no>
2020-07-22 12:23:26 +0200

    First pipeline

---
+++ build-tools/001-configure-and-build/configure-and-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Restore CMake from cache
+cache restore "${CMAKE_CACHE_STRING}"
+
+# Configure build
+"./cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake" -B build -DCMAKE_BUILD_TYPE=Release .
+
+# Build
+"./cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake" --build build -j 2
